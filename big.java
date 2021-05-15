2f6220cc708a1c1b24458edf3b853c8befcdd107
priyank awasthi <priyankawasthi252@gmail.com>
2021-05-15 18:07:04 +0530

    hello

---
+++ big.java
@@ -0,0 +1,15 @@
+class big
+{
+public static void main(String[] args)
+{
+int a[][]={23,12,6,18,9};
+int big=0;
+max=a.length;
+for (int i=0;i<2;i++)
+{
+for(int j=0;j<2;j++)
+{
+System.out.println("biggest number is:",+a[i][j]);
+}
+}
+}
